cd06e443b9b5cdcdc8c1733dc64cdf1102b26544
Inés María Oliveros H <ines.oliveros@symplifica.com>
2024-02-02 11:40:31 -0500

    implementation methods edit and show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,31 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-    @article.title = 'Demo'
-  end
 
-  def create
-    @article = Article.new(article_params)
+	def show
+		@article = Article.find(params[:id])
+	end
 
-    if @article.save
-      redirect_to bienvenida_path, notice: 'Article was successfully created.'
-    else
-      render :new
-    end
-  end
+	def new
+		@article = Article.new
+	end
 
-  private
+	def create
+		@article = Article.new(article_params)
 
-  def article_params
-    params.require(:article).permit(:title, :content)
-  end
+		if @article.save
+		  redirect_to bienvenida_path, notice: 'Article was successfully created.'
+		else
+		  render :new
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
 end
 
-
-
-# class ArticlesController < ApplicationController
-#   def new
-#     @article = Article.new
-#     @article.title = 'Demo'
-#   end
-
-#   def create
-#     @article = Article.new(article_params)
-
-#     if @article.save
-#       redirect_to bienvenida_path(article: @article.id), notice: 'Article was successfully created.'
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def article_params
-#     params.require(:article).permit(:title, :content)
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get "bienvenida", to: "home#index"
-  resources :articles  
+  #resources :articles  
   
   get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+
+  put "/articles/:id", to: "articles#update", as: :article
   post "articles", to: "articles#create"
 
 end
